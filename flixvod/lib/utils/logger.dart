cbfce5c5e11b0a4e612b17358739a867e4131c9f
David Perez <>
2025-07-06 15:25:45 +0200

    chore: Moved logger to utils

---
+++ flixvod/lib/utils/logger.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+
+class FlixLogger {
+  FlixLogger._();
+
+  static final FlixLogger _instance = FlixLogger._();
+  static FlixLogger get instance => _instance;
+  
+  static FlixLogger get I => _instance;
+  
+  void d(dynamic message) {
+    if (kDebugMode) {
+      debugPrint('[FlixVOD] ${message.toString()}');
+    }
+  }
+  
+  void e(dynamic message, [StackTrace? stackTrace]) {
+    if (kDebugMode) {
+      debugPrint('[FlixVOD ERROR] ${message.toString()}');
+      if (stackTrace != null) {
+        debugPrint('[FlixVOD STACK] $stackTrace');
+      }
+    }
+  }
+
+  void w(dynamic message) {
+    if (kDebugMode) {
+      debugPrint('[FlixVOD WARN] ${message.toString()}');
+    }
+  }
+
+  void i(dynamic message) {
+    if (kDebugMode) {
+      debugPrint('[FlixVOD INFO] ${message.toString()}');
+    }
+  }
+}
+
+final logger = FlixLogger.instance;
