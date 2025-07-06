8559c3ae60792d726753c4f5226ee1d988a06bdb
David Perez <>
2025-07-06 18:04:01 +0200

    catalogue: Removed filter chips from bar

--- flixvod/lib/modules/catalogue/widgets/catalogue_app_bar_widget.dart
+++ flixvod/lib/modules/catalogue/widgets/catalogue_app_bar_widget.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../localization/localized.dart';
-import '../../../models/media.dart';
-import '../bloc/catalogue_bloc.dart';
-import '../bloc/catalogue_state.dart';
-import '../bloc/catalogue_event.dart';
 import 'search_bar_widget.dart';
-import 'filter_chips_widget.dart';
 import '../../../core/app_theme.dart';
 import '../../../core/app_icons.dart';
 
@@ -14,28 +8,32 @@ class CatalogueAppBarWidget extends StatelessWidget implements PreferredSizeWidg
   final TextEditingController searchController;
   final ValueChanged<String> onSearchChanged;
   final VoidCallback onUpload;
+  final FocusNode? searchFocusNode;
 
   const CatalogueAppBarWidget({
     super.key,
     required this.searchController,
     required this.onSearchChanged,
     required this.onUpload,
-  });
-
-  @override
+    this.searchFocusNode,
+  });  @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text(Localized.of(context).catalogue),
+      title: Text(
+        Localized.of(context).appTitle,
+      ),
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      foregroundColor: AppTheme.primaryForegroundColor,
       actions: [
         IconButton(
           onPressed: onUpload,
           icon: AppIcons.add,
           tooltip: Localized.of(context).uploadMedia,
+          color: AppTheme.primaryForegroundColor,
         ),
       ],
       bottom: PreferredSize(
-        preferredSize: const Size.fromHeight(160),
+        preferredSize: const Size.fromHeight(80),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -43,58 +41,9 @@ class CatalogueAppBarWidget extends StatelessWidget implements PreferredSizeWidg
             SearchBarWidget(
               controller: searchController,
               onChanged: onSearchChanged,
+              focusNode: searchFocusNode,
             ),
             AppTheme.smallVerticalSpacer,
-            // Filter Chips for Media Types
-            BlocBuilder<CatalogueBloc, CatalogueState>(
-              builder: (context, state) {
-                // Extract available media types from all media
-                final availableTypes = state.allMedia
-                    .map((media) => media.type)
-                    .toSet()
-                    .toList();
-
-                return FilterChipsWidget<MediaType>(
-                  availableOptions: availableTypes,
-                  selectedOption: state.selectedFilter,
-                  onFilterChanged: (type) {
-                    context.read<CatalogueBloc>().add(FilterByType(type));
-                  },
-                  getOptionLabel: (context, type) {
-                    switch (type) {
-                      case MediaType.movie:
-                        return Localized.of(context).movies;
-                      case MediaType.series:
-                        return Localized.of(context).series;
-                    }
-                  },
-                  allLabel: Localized.of(context).all,
-                );
-              },
-            ),
-            AppTheme.tinyVerticalSpacer,
-            // Filter Chips for Genres
-            BlocBuilder<CatalogueBloc, CatalogueState>(
-              builder: (context, state) {
-                // Extract available genres from all media
-                final availableGenres = state.allMedia
-                    .expand((media) => media.genres)
-                    .toSet()
-                    .toList()
-                    ..sort(); // Sort alphabetically
-
-                return FilterChipsWidget<String>(
-                  availableOptions: availableGenres,
-                  selectedOption: state.selectedGenre,
-                  onFilterChanged: (genre) {
-                    context.read<CatalogueBloc>().add(FilterByGenre(genre));
-                  },
-                  getOptionLabel: (context, genre) => genre,
-                  allLabel: Localized.of(context).allGenres,
-                );
-              },
-            ),
-            AppTheme.specialVerticalSpacer,
           ],
         ),
       ),
@@ -102,5 +51,5 @@ class CatalogueAppBarWidget extends StatelessWidget implements PreferredSizeWidg
   }
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 160);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight + 80);
 }
